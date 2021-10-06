dae694891b25455e0334a218b3a69ddafe8ab5a3
Renan Bernardo_CONTR <c0639672@vale.com>
2021-10-06 08:37:28 -0300

    Add project files.

---
+++ src/DevIo.AppMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevIo.AppMvc.Startup))]
+namespace DevIo.AppMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
