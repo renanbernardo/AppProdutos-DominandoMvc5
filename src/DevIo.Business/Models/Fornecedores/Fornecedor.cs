48ce5ca5c699b3b1342104e9467393541913406e
Renan Bernardo_CONTR <c0639672@vale.com>
2021-10-06 08:51:48 -0300

    Definindo o conceito de Entidade

---
+++ src/DevIo.Business/Models/Fornecedores/Fornecedor.cs
@@ -0,0 +1,8 @@
+﻿using DevIo.Business.Core.Models;
+
+namespace DevIo.Business.Models.Fornecedores
+{
+    public class Fornecedor : Entity
+    {
+    }
+}
---
+++ src/DevIo.Business/Models/Produtos/Produto.cs
@@ -0,0 +1,8 @@
+﻿using DevIo.Business.Core.Models;
+
+namespace DevIo.Business.Models.Produtos
+{
+    public class Produto : Entity
+    {
+    }
+}
