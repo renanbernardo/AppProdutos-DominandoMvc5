22e91d9a7f05d8885e37560f89f83b957d66ba56
Renan Bernardo <c0639672@vale.com>
2021-10-07 07:55:03 -0300

    Escrevendo Entidades de Negócio

--- src/DevIo.Business/Models/Fornecedores/Fornecedor.cs
+++ src/DevIo.Business/Models/Fornecedores/Fornecedor.cs
@@ -1,8 +1,18 @@
 ﻿using DevIo.Business.Core.Models;
+using DevIo.Business.Models.Produtos;
+using System.Collections.Generic;
 
 namespace DevIo.Business.Models.Fornecedores
 {
     public class Fornecedor : Entity
     {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        //EF Relations
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
--- src/DevIo.Business/Models/Produtos/Produto.cs
+++ src/DevIo.Business/Models/Produtos/Produto.cs
@@ -1,8 +1,20 @@
 ﻿using DevIo.Business.Core.Models;
+using DevIo.Business.Models.Fornecedores;
+using System;
 
 namespace DevIo.Business.Models.Produtos
 {
     public class Produto : Entity
     {
+        public Guid FornecedorId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Imagem { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public Fornecedor Fornecedor { get; set; }
     }
 }
