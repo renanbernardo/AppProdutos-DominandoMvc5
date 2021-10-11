62f1f7b8e2d67aa0e480107d1803c713a644c89a
renanbernardo <renan.brnrd@gmail.com>
2021-10-11 16:47:16 -0300

    Validação com FluentValidation

---
+++ src/DevIo.Business/Models/Fornecedores/Validations/FornecedorValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace DevIo.Business.Models.Fornecedores.Validations
+{
+    public class FornecedorValidation : AbstractValidator<Fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(f => f.Nome)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser preenchido.")
+                .Length(3, 200)
+                .WithMessage("O campo {PropertyName} precisa ter entre {MinLength} e {MaxLength} caracteres.");
+        }
+    }
+}
