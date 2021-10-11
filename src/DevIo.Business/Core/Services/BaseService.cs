0ffc5c06dd77a3ff2fbb6236e4378e3131af5fa7
renanbernardo <renan.brnrd@gmail.com>
2021-10-11 18:31:45 -0300

    Implementando serviços de negócios

---
+++ src/DevIo.Business/Core/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using DevIo.Business.Core.Models;
+using FluentValidation;
+
+namespace DevIo.Business.Core.Services
+{
+    public abstract class BaseService
+    {
+        protected bool ExecutarValidacao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE : Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            return validator.IsValid;
+        }
+    }
+}
