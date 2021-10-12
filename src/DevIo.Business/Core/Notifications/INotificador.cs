b8dc013f401845aff532314e944208ea01e810b7
renanbernardo <renan.brnrd@gmail.com>
2021-10-12 13:51:23 -0300

    Notificação de erros

---
+++ src/DevIo.Business/Core/Notifications/INotificador.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DevIo.Business.Core.Notifications
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
