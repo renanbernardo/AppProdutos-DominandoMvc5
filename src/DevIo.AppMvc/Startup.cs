20cd502774156d4ef6248d21f01ddeef1dd75b79
renanbernardo <renan.brnrd@gmail.com>
2021-11-03 08:34:37 -0300

    Utilizandoo Startup.cs da forma certa

--- src/DevIo.AppMvc/Startup.cs
+++ src/DevIo.AppMvc/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using DevIo.AppMvc.App_Start;
+using Microsoft.Owin;
 using Owin;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
 
 [assembly: OwinStartupAttribute(typeof(DevIo.AppMvc.Startup))]
 namespace DevIo.AppMvc
@@ -9,6 +13,13 @@ namespace DevIo.AppMvc
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            DependencyInjectionConfig.RegisterDIContainer();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
