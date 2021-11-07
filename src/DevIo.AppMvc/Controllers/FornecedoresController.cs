3b38fdc67c9e1c8e6e9c2a259046b0503dee228c
renanbernardo <renan.brnrd@gmail.com>
2021-11-07 12:06:34 -0300

    Implementando Views de Fornecedores

--- src/DevIo.AppMvc/Controllers/FornecedoresController.cs
+++ src/DevIo.AppMvc/Controllers/FornecedoresController.cs
@@ -30,6 +30,19 @@ namespace DevIo.AppMvc.Controllers
             return View(_mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos()));
         }
 
+        [Route("dados-do-fornecedor/{id:guid}")]
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(fornecedorViewModel);
+        }
+
         [Route("novo-fornecedor")]
         public ActionResult Create()
         {
