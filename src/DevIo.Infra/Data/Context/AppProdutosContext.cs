230b60edb0098634ab25919c4227e5a995d377b4
renanbernardo <renan.brnrd@gmail.com>
2021-11-13 12:58:50 -0300

    Definindo Data Automática no EF

--- src/DevIo.Infra/Data/Context/AppProdutosContext.cs
+++ src/DevIo.Infra/Data/Context/AppProdutosContext.cs
@@ -7,6 +7,7 @@ using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DevIo.Infra.Data.Context
@@ -39,6 +40,24 @@ namespace DevIo.Infra.Data.Context
             modelBuilder.Configurations.Add(new ProdutoConfig());
 
             base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
