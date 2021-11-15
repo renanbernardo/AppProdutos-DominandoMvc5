840a443bbae5416de73395f2685b5811cfcfa7b1
renanbernardo <renan.brnrd@gmail.com>
2021-11-14 22:40:52 -0300

    Tratamento de Erros

---
+++ src/DevIo.AppMvc/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DevIo.AppMvc.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public int ErrorCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
